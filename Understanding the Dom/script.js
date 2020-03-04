abfde17c4c5690cdcb65d94767535c6b38100832
Agbamoro Victoria <45354743+Victoriaagbamoro@users.noreply.github.com>
2020-03-04 10:18:09 +0100

    Add files via upload

---
+++ Understanding the Dom/script.js
@@ -0,0 +1,7 @@
+function changeColor(newColor){
+    var colorElement = document.getElementById('para');
+    colorElement.style.color = newColor;
+    // newColor.style.style  ='3px solid lightseagreen';
+    
+}
+
